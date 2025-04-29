0d4073f6c3baaf13c58ff8ca98cd48751c626207
Manmohan Wable <manmohanwable0@gmail.com>
2025-04-29 10:45:29 +0530

    Backend linked to Auth_Service microservice

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -1,48 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid"
 import { cookies } from "next/headers";
-import amqplib from "amqplib"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import axios from "axios";
 
 dotenv.config()
 
 export async function POST(req: NextRequest) {
     const msg = await req.json()
-    const correlationId = uuidv4()
-
-    const RabbitMQ = await amqplib.connect('amqp://localhost')
-
-    const channel = await RabbitMQ.createChannel()
-
-    const replyQueue = await channel.assertQueue('', { exclusive: true })
-    channel.assertQueue("SignIn", { durable: true })
-
-    channel.sendToQueue("SignIn", Buffer.from(JSON.stringify(msg)), { replyTo: replyQueue.queue, correlationId: correlationId })
-
-    const response = await new Promise(resolve => {
-        channel.consume(replyQueue.queue, async (recieved_msg) => {
-            if (recieved_msg?.properties.correlationId === correlationId) {
-                const reason = JSON.parse(recieved_msg.content.toString())
-
-                if (reason.success && reason.reason == "") {
-                    const Cookies = await cookies()
-                    const token = jwt.sign(JSON.stringify(reason.user), process.env.SECRET_KEY ?? "")
-                    Cookies.set("token", token)
-
-                    resolve({ success: true, reason: "" })
-                } else if (!reason.success && reason.reason == "Internal Server Error") {
-                    resolve({ success: false, reason: "Internal Server Error" })
-                } else if (!reason.success && reason.reason == "User Not Found") {
-                    resolve({ success: false, reason: "User does not exist" })
-                } else if (!reason.success && reason.reason == "Incorrect Password") {
-                    resolve({ success: false, reason: "Incorrect Password"})
-                } else if (!reason.success && reason.reason == "Wrong Format") {
-                    resolve({ success: false, reason: "Wrong Format", issue: reason.issues[0].message })
-                }
+            
+        const response = await new Promise(async resolve => {
+            
+            const response = await axios.post("http://localhost:3001/auth/signin", msg)
+            const data = response.data
+            
+            if (data.success && data.reason == "") {
+                const Cookies = await cookies()
+                const token = jwt.sign(JSON.stringify(data.user), process.env.SECRET_KEY ?? "")
+                Cookies.set("token", token)
+                resolve({ success: true, reason: "" })
+            } else if (!data.success && data.reason == "Internal Server Error") {
+                resolve({ success: false, reason: "Internal Server Error" })
+            } else if (!data.success && data.reason == "User Not Found") {
+                resolve({ success: false, reason: "User does not exist" })
+            } else if (!data.success && data.reason == "Incorrect Password") {
+                console.log("Incorrect Password")
+                resolve({ success: false, reason: "Incorrect Password"})
+            } else if (!data.success && data.reason == "Wrong Format") {
+                resolve({ success: false, reason: "Wrong Format", issue: data.issues[0].message })
             }
+            
         })
-    })
-
     return NextResponse.json(response)
 }
